a2f6a1ab099b439abb519b39ee4bfa42108547bb
abhishekbhartiind <imabhishekbharti12@gmail.com>
2022-02-26 21:01:05 +0530

    working on contracts

---
+++ contracts/NFT.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+}
